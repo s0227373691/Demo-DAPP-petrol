9ae768c792255f76bc8531cef298a75778c0b321
Elmer <s0227373691@gms.tku.edu.tw>
2023-01-20 13:56:57 +0800

    added sell petrol function .

--- constracts/pettrol.sol
+++ constracts/pettrol.sol
@@ -5,12 +5,14 @@ pragma solidity ^0.8.0;
 contract petrolExchang{
     //  owner
     address public owner;
+    address public deployAddress;
 
     // petrolBalence
     mapping(address => uint256) public petrolBalences; 
 
     constructor(){
         owner = msg.sender;
+        deployAddress = address(this);
         petrolBalences[address(this)] = 1000;
     }
 
@@ -24,10 +26,19 @@ contract petrolExchang{
     }
 
     function buyPetrol(uint256 amount) public payable{
-          require(msg.value >=  amount * 0.01 ether,"not enough money to buy petrol");
-          require(petrolBalences[address(this)] >= amount, "not enough petrol to bought");
+        require(msg.value >=  amount * 1 ether,"not enough money to buy petrol");
+        require(petrolBalences[address(this)] >= amount, "not enough petrol to bought");
 
-          petrolBalences[address(this)] -= amount;
-          petrolBalences[msg.sender] += amount;
+        petrolBalences[address(this)] -= amount;
+        petrolBalences[msg.sender] += amount;
+    }
+
+    function sellPetrol(uint256 amount) public payable{
+        require(petrolBalences[msg.sender] >= amount,"not enough petrol to sell");
+        require(address(this).balance >= 1, "not enough petrol to bought");
+
+        petrolBalences[address(this)] += amount;
+        petrolBalences[msg.sender] -= amount;
+        payable(msg.sender).transfer(1 ether * amount);
     }
 }
